8e552f1e3fd8b3bc70f020aec61e7808840b0fee
guanhao <belive68@foxmail.com>
2019-09-03 18:47:20 +0800

    update code

--- src/thread/synchronize/SynchronizedTest.java
+++ src/thread/synchronize/SynchronizedTest.java
@@ -20,10 +20,12 @@ public class SynchronizedTest {
     }
 
     /**
-     * 两个不同的对象，方法上加锁，锁住了对象实例，所以不会产生资源的抢占。
+     * 两个不同的对象，方法上加锁，锁住了对象实例，即使是同一个方法，也不会产生资源的抢占。
+     *
+     * 关键在于 上锁的对象是否被抢占
      *
      */
-    public static void main1(String[] args) {
+    public static void main(String[] args) {
         new Thread(() -> {
             SynchronizedTest test = new SynchronizedTest();
             test.method1("a");
@@ -36,10 +38,12 @@ public class SynchronizedTest {
     }
 
     /**
-     * 同一个对象，方法上锁，锁住了对象实例，所以会产生资源的抢占。
+     * 同一个对象，方法上加锁，锁住了对象实例，即使是不同的方法，也会产生资源的抢占。
+     *
+     * 关键在于 上锁的对象是否被抢占
      *
      */
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         SynchronizedTest test = new SynchronizedTest();
         new Thread(() -> {
             test.method1("a");

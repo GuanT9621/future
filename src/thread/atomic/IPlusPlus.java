f70a6aef002fe0c8635b753660b1a1ef46c91615
guanhao <belive68@foxmail.com>
2019-09-05 23:55:57 +0800

    volatile

--- src/thread/atomic/IPlusPlus.java
+++ src/thread/atomic/IPlusPlus.java
@@ -1,13 +1,20 @@
 package thread.atomic;
 
+/**
+ * 输出结果不为50000
+ * 因为 volatile 仅仅保证对单个volatile变量的读/写具有原子性
+ * 所以x++这种先读取，在计算，在写入的操作，并不能依靠volatile获得原子性。需要依靠synchronized (IPlusPlus.class) {} 锁定方法块实现原子性。
+ */
 public class IPlusPlus {
 
     static volatile int x, y;
 
     static class IncThread extends Thread {
         public void run() {
-            for (int i = 0; i < 50000; i++) x++;
-            for (int i = 0; i < 50000; i++) y = y+1;
+            for (int i = 0; i < 50000; i++) {
+                x++;
+                y = y + 1;
+            }
         }
     }
 

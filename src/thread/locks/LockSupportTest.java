82f3a61bd0a9a40400cc14c9c05d4d9c8827cfc3
guanhao <belive68@foxmail.com>
2019-09-06 20:40:15 +0800

    noting

--- src/thread/locks/LockSupportTest.java
+++ src/thread/locks/LockSupportTest.java
@@ -45,7 +45,6 @@ public class LockSupportTest {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
                 }
             });
             b.start();

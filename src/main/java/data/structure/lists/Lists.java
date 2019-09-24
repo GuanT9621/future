8c55a42732013f58b53e72b30c55e48f386835b8
guanhao <belive68@foxmail.com>
2019-09-24 20:34:21 +0800

    add arraylist

--- src/main/java/data/structure/lists/Lists.java
+++ src/main/java/data/structure/lists/Lists.java
@@ -5,11 +5,28 @@ import java.util.List;
 
 public class Lists {
 
+
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(50);
-        list.add("1");
-        System.out.println("list size : " + list.size());
-        System.out.println("list 33 element : " + list.get(33));
+
+        /**
+         * ArrayList有无初始化的差别
+         */
+        int range = 10000000;
+
+        List<Integer> initialList = new ArrayList<>(range);
+        long s1 = System.nanoTime();
+        for (int i=0; i<range; i++) {
+            initialList.add(i);
+        }
+        System.out.println(System.nanoTime() - s1);
+
+//        List<Integer> list = new ArrayList<>();
+//        long s2 = System.nanoTime();
+//        for (int i=0; i<range; i++) {
+//            list.add(i);
+//        }
+//        System.out.println(System.nanoTime() - s2);
+
     }
 
 }

1093f94df3d5817d875de421b1a83fdb28acebbc
guanhao <belive68@foxmail.com>
2019-10-28 16:11:48 +0800

    add sort of top-heap

--- src/main/java/data/structure/heap/TopHeap.java
+++ src/main/java/data/structure/heap/TopHeap.java
@@ -21,13 +21,20 @@ import java.lang.reflect.Array;
  *
  * 公式：array[i] >= array[2i + 1] && array[i] >= array[2i + 2]
  *
- * 其实也可以用堆排序实现大顶堆
+ * 堆排序的基本思路：
+ * a.将无需序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆;
+ * b.将堆顶元素与末尾元素交换，将最大元素"沉"到数组末端;
+ * c.重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素，反复执行调整+交换步骤，直到整个序列有序。
+ * 其实就是每次拿走一下堆顶。（可以不用递归了，因为堆排序只需要堆顶就够了）
+ *
  */
 public class TopHeap {
-    private int[] array;
 
-    public TopHeap(int[] array) {
-        this.array = array;
+    public static void sort(int[] array) {
+        for (int i=0; i < array.length; i++) {
+            reHeap(array, array.length - 1 - i);
+            swap(array, 0, array.length - 1 - i);
+        }
     }
 
     /**
@@ -39,58 +46,60 @@ public class TopHeap {
      *
      * 3 提取出 "父节点比较左子/右子节点并交换" 作为独立方法，递归调用，即可实现"完全最大堆"
      */
-    public synchronized void reHeap() {
-        int allIndex = array.length - 1;
-        int endIndex; //最后一个非叶子节点的位置
+    public static void reHeap(int[] array, int endIndex) {
+        //最后一个非叶子节点的位置
+        int lastNonLeafIndex;
         // 左奇数，右偶数
-        if (allIndex / 2 == 0) {
-            endIndex = (allIndex - 2) / 2;
+        if (endIndex / 2 == 0) {
+            lastNonLeafIndex = (endIndex - 2) / 2;
         } else {
-            endIndex = (allIndex - 1) / 2;
+            lastNonLeafIndex = (endIndex - 1) / 2;
         }
-        for (int i = endIndex; i >= 0; i--) {
-            reSub(i);
+        for (int i = lastNonLeafIndex; i >= 0; i--) {
+            reSub(array, i, endIndex);
         }
     }
-    private void reSub(int i) {
-        if ((2*i + 1) > array.length - 1)  {
+    private static void reSub(int[] array, int i, int endIndex) {
+        if ((2*i + 1) > endIndex)  {
             return;
         }
         if (array[2*i + 1] > array[i]) {
             swap(array, i, 2 * i + 1);
-            reSub(2 * i + 1);
+            reSub(array,2 * i + 1, endIndex);
+        }
+        if ((2*i + 2) > endIndex)  {
+            return;
         }
         if (array[2*i + 2] > array[i]) {
             swap(array, i, 2 * i + 2);
-            reSub(2 * i + 2);
+            reSub(array, 2 * i + 2, endIndex);
         }
     }
-    private void swap(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    public synchronized boolean compareAndAdd(int in) {
+    public static boolean compareAndAdd(int[] array, int in) {
         if (in > array[0]) { return false; }
         array[0] = in;
-        reHeap();
+        reHeap(array, array.length - 1);
         return true;
     }
 
     public static void main(String[] args) {
         int[] array = new int[]{20, 25, 35, 30, 10, 15, 40, 45, 50};
-
-        TopHeap topHeap = new TopHeap(array);
-        topHeap.reHeap();
-        for (int i: array) {
-            System.out.print(i + ", ");
-        }
+        TopHeap.reHeap(array, array.length - 1);
+        for (int i: array) { System.out.print(i + ", "); }
 
         System.out.println();
-        topHeap.compareAndAdd(24);
-        for (int i: array) {
-            System.out.print(i + ", ");
-        }
+        TopHeap.compareAndAdd(array, 24);
+        for (int i: array) { System.out.print(i + ", "); }
+
+        System.out.println();
+        int[] unsorted = new int[]{20, 25, 35, 30, 10, 15, 40, 45, 50, 100};
+        TopHeap.sort(unsorted);
+        for (int i: unsorted) { System.out.print(i + ", "); }
     }
 }
